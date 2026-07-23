daf4258d90209568cde4ec69755bc3cd3075647b
xiaosa-zhz <56922836+xiaosa-zhz@users.noreply.github.com>
2026-07-23 12:09:48 +0800

    Add some operators and use wide contracts

--- bit_flag.hh
+++ bit_flag.hh
@@ -1,9 +1,10 @@
 #pragma once
 
-#include <concepts>
+#include <cstddef>
 #include <cstdint>
-#include <limits>
+#include <concepts>
 #include <meta>
+#include <limits>
 #include <utility>
 #include <initializer_list>
 #include <ranges>
@@ -25,7 +26,7 @@ consteval std::size_t max_enumerator_value() {
     auto [min, max] = std::ranges::minmax(enumerators
         | std::views::transform([](auto i) { return std::to_underlying(extract<Enum>(i)); })
     );
-    if constexpr (std::is_signed_v<Enum>) {
+    if constexpr (std::is_signed_v<std::underlying_type_t<Enum>>) {
         if (min < 0) {
             throw std::meta::exception("negative", ^^Enum);
         }
@@ -34,7 +35,7 @@ consteval std::size_t max_enumerator_value() {
 }
 
 template<typename Enum>
-using default_underlying_type = [:[] consteval {
+using default_representation_type = [:[] consteval {
     static constexpr std::size_t max = max_enumerator_value<Enum>();
     if (max < 8) return ^^decltype(std::uint8_t{});
     if (max < 16) return ^^decltype(std::uint16_t{});
@@ -45,31 +46,50 @@ using default_underlying_type = [:[] consteval {
 
 } // namespace mylib::details
 
-template<typename Enum, typename UnderlyingType = details::default_underlying_type<Enum>>
+// Enum values are used as indices of the bit flag.
+// Enum values not presented in definition are ignored.
+template<typename Enum, typename RepType = details::default_representation_type<Enum>>
 class bit_flag
 {
     static_assert(std::is_enum_v<Enum>);
-    static_assert(std::is_unsigned_v<UnderlyingType>);
-    static_assert(std::numeric_limits<UnderlyingType>::digits > details::max_enumerator_value<Enum>());
+    static_assert(std::is_integral_v<RepType> && std::is_unsigned_v<RepType> && !std::is_same_v<RepType, bool>);
+    static_assert(std::size_t(std::numeric_limits<RepType>::digits) > details::max_enumerator_value<Enum>());
 
 public:
     using enum_type = Enum;
-    using underlying_type = UnderlyingType;
+    using representation_type = RepType;
 
 private:
-    static constexpr underlying_type zero = 0;
-    static constexpr underlying_type one = 1;
-    static constexpr bit_flag all_set = [] consteval {
-        return enumerators_of(^^enum_type)
-            | std::views::transform(&std::meta::extract<enum_type>)
-            | std::ranges::to<bit_flag<enum_type, underlying_type>>();
+    static constexpr representation_type zero = 0;
+    static constexpr representation_type one = 1;
+    static constexpr representation_type all_set = [] consteval {
+        representation_type rt = 0;
+        for (auto info : enumerators_of(^^enum_type)) {
+            rt |= (one << std::to_underlying(extract<enum_type>(info)));
+        }
+        return rt;
     }();
+    static constexpr bit_flag all_set_flag = from_representation(all_set);
+
+    static constexpr bool filter_unchecked_enum(enum_type e) noexcept {
+        auto val = std::to_underlying(e);
+        if constexpr (std::is_signed_v<decltype(val)>) {
+            if (val < 0) {
+                return false;
+            }
+        }
+        return val <= details::max_enumerator_value<enum_type>();
+    }
 
 public:
     constexpr bit_flag() = default;
     constexpr bit_flag(const bit_flag&) = default;
     constexpr bit_flag& operator=(const bit_flag&) = default;
-    constexpr bit_flag(enum_type e) noexcept : rep(one << std::to_underlying(e)) {}
+
+    constexpr bit_flag(enum_type e) noexcept
+        : bit_flag(from_representation(filter_unchecked_enum(e) ? one << std::to_underlying(e) : zero))
+    {}
+
     constexpr bit_flag(std::initializer_list<enum_type> il) noexcept : bit_flag(std::from_range, il) {}
 
     template<std::ranges::input_range R>
@@ -80,33 +100,42 @@ public:
         }
     }
 
-    static constexpr bit_flag from_underlying(underlying_type rep) noexcept {
+    [[nodiscard]] static constexpr bit_flag from_representation(representation_type rep) noexcept {
         bit_flag b;
-        b.rep = rep;
+        b.rep = rep & all_set;
         return b;
     }
 
-    constexpr underlying_type to_underlying() const noexcept { return rep; }
+    [[nodiscard]] constexpr representation_type to_representation() const noexcept { return rep; }
 
     constexpr bit_flag& set(bit_flag other) noexcept { rep |= other.rep; return *this; }
-    constexpr bit_flag& set() noexcept { return set(all_set); }
+    constexpr bit_flag& set() noexcept { return set(all_set_flag); }
     constexpr bit_flag& reset(bit_flag other) noexcept { rep &= ~other.rep; return *this; }
-    constexpr bit_flag& reset() noexcept { return reset(all_set); }
+    constexpr bit_flag& reset() noexcept { return reset(all_set_flag); }
     constexpr bit_flag& flip(bit_flag other) noexcept { rep ^= other.rep; return *this; }
-    constexpr bit_flag& flip() noexcept { return flip(all_set); }
-    constexpr bool all_of(bit_flag other) const noexcept { return (rep & other.rep) == other.rep; }
-    constexpr bool any_of(bit_flag other) const noexcept { return (rep & other.rep) != zero; }
-    constexpr bool none_of(bit_flag other) const noexcept { return (rep & other.rep) == zero; }
-    constexpr bool all() const noexcept { return all_of(all_set); }
-    constexpr bool any() const noexcept { return any_of(all_set); }
-    constexpr bool none() const noexcept { return none_of(all_set); }
-    constexpr bool test(enum_type e) const noexcept { return any_of(e); }
-    constexpr std::size_t count() const noexcept { return std::popcount(rep); }
+    constexpr bit_flag& flip() noexcept { return flip(all_set_flag); }
+    [[nodiscard]] constexpr bool all_of(bit_flag other) const noexcept { return (rep & other.rep) == other.rep; }
+    [[nodiscard]] constexpr bool any_of(bit_flag other) const noexcept { return (rep & other.rep) != zero; }
+    [[nodiscard]] constexpr bool none_of(bit_flag other) const noexcept { return (rep & other.rep) == zero; }
+    [[nodiscard]] constexpr bool all() const noexcept { return all_of(all_set_flag); }
+    [[nodiscard]] constexpr bool any() const noexcept { return any_of(all_set_flag); }
+    [[nodiscard]] constexpr bool none() const noexcept { return none_of(all_set_flag); }
+    [[nodiscard]] constexpr bool test(enum_type e) const noexcept { return any_of(e); }
+    [[nodiscard]] constexpr std::size_t count() const noexcept { return std::popcount(rep); }
 
-    friend constexpr bool operator==(bit_flag lhs, bit_flag rhs) = default;
+    constexpr bit_flag& operator|=(bit_flag other) noexcept { rep |= other.rep; return *this; }
+    constexpr bit_flag& operator&=(bit_flag other) noexcept { rep &= other.rep; return *this; }
+    constexpr bit_flag& operator^=(bit_flag other) noexcept { rep ^= other.rep; return *this; }
+
+    [[nodiscard]] friend constexpr bit_flag operator|(bit_flag lhs, bit_flag rhs) noexcept { return lhs |= rhs; }
+    [[nodiscard]] friend constexpr bit_flag operator&(bit_flag lhs, bit_flag rhs) noexcept { return lhs &= rhs; }
+    [[nodiscard]] friend constexpr bit_flag operator^(bit_flag lhs, bit_flag rhs) noexcept { return lhs ^= rhs; }
+    [[nodiscard]] friend constexpr bit_flag operator~(bit_flag f) noexcept { return f.flip(); }
+
+    [[nodiscard]] friend constexpr bool operator==(bit_flag lhs, bit_flag rhs) = default;
 
 private:
-    underlying_type rep = zero;
+    representation_type rep = zero;
 };
 
 template<std::ranges::input_range R>
@@ -115,9 +144,9 @@ bit_flag(std::from_range_t, R&& r) -> bit_flag<std::ranges::range_value_t<R>>;
 
 } // namespace mylib
 
-template<typename Enum, typename UnderlyingType>
-struct std::hash<mylib::bit_flag<Enum, UnderlyingType>> {
-    static std::size_t operator()(mylib::bit_flag<Enum, UnderlyingType> b) noexcept {
-        return std::hash<UnderlyingType>{}(b.to_underlying());
+template<typename Enum, typename RepType>
+struct std::hash<mylib::bit_flag<Enum, RepType>> {
+    constexpr std::size_t operator()(mylib::bit_flag<Enum, RepType> b) const noexcept {
+        return std::hash<RepType>{}(b.to_representation());
     }
 };
